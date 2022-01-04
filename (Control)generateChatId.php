80007f0ec1f2b7329a50604eed46bd12b2f8d22e
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-04 14:57:18 +0200

    Update (Control)generateChatId.php

--- (Control)generateChatId.php
+++ (Control)generateChatId.php
@@ -9,7 +9,7 @@ require './vendor/autoload.php';
 require '(Model)getUsername.inc.php';
 
 $usernameForChat=$xx['username'];
-$server_client = new Client("swtmb3ttxxsq", "cgak3n5gnczyva9e5hnzgsqwfg2xz5a7v9sy6xvu3eryebrs7xbysxhhfhb5tajg"); //stable
+$server_client = new Client("z5j34vkctqrq", "zad2a8rmxswdce7ay4wv4xjsbwue4tzpjmkb8vex2aqzjpc646n6ttsc7j727nkn"); //stable for account krowl
 $chat_id = $server_client->createToken("'.$usernameForChat.'");
 echo $chat_id;
 require '(View)generateChatId.php';
