e3c2d9594a51e5518afd2c9b0e1422f2db93513b
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-06 15:58:59 +0200

    ggg

--- (Model)leftOccupants.inc.php
+++ (Model)leftOccupants.inc.php
@@ -1,10 +1,8 @@
 <?php 
 require '(Model)config.inc.php';
 $con=con();
-// $sql="DELETE FROM `occupants` WHERE `user_id`=(SELECT `user_id` FROM `user` WHERE `username`='".$nameLeft."')";
-// $yy=mysqli_query($con,$sql);
-
-$sql="INSERT INTO `leftoccupants`(`id_leftoccupant`, `name`) VALUES (NULL,'".$nameLeft."')";
+$sql="DELETE FROM `occupants` WHERE `user_id`=(SELECT `user_id` FROM `user` WHERE `username`='".$nameLeft."')";
 $yy=mysqli_query($con,$sql);
+
 
 ?>
