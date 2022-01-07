b9423f84590e06b01a357ae2b1ea5be642c4837f
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-07 15:12:40 +0200

    Update (Control)createPost.php

--- (Control)createPost.php
+++ (Control)createPost.php
@@ -1,7 +1,7 @@
 <?php 
 require '(Control)versionTest.php'; 
 
-//if(require '(Control)tokenCheck.php'){ 
+if(require '(Control)tokenCheck.php'){ 
     
 //creating a Post 
 $post_array = array();
@@ -14,31 +14,11 @@ if(!empty($data->account_Id) && !empty($data->post_subject) && !empty($data->pos
     $post_context = htmlspecialchars($data->post_context);
 	
     $json_array[0] = 'error4';
-    $json_array[1] = 'tokennn';
 
-
-    require '(Model)config.inc.php';
-    $con=con($server);
- 
     require '(Model)createPost.inc.php'; 
-    if(mysqli_num_rows($yy)>0){
-        $res = mysqli_fetch_assoc($yy);
-    	
+    if($yy){
         $json_array[0] = 'success';
-        $post_array[] = array(
-                            $res["post_tag"],
-                            $res["post_question"],
-                            //$res["post_context"], 
-                            $res["post_val"],
-                            $res["post_date"],
-                            );
-    
-    }else{
-        $table_array[]=[];
     }
-
-    //$json_array[1] = $token;
-    $json_array[2] = $post_array;
 
         echo json_encode($json_array);
 
@@ -48,5 +28,5 @@ if(!empty($data->account_Id) && !empty($data->post_subject) && !empty($data->pos
 
 
 
-//}
+}
 ?>
