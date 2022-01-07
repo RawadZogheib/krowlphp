55f283c4bcd2fe68b9018783b2a79e6f1f564a88
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-07 14:53:17 +0200

    Update (Model)loadPosts.inc.php

--- (Model)loadPosts.inc.php
+++ (Model)loadPosts.inc.php
@@ -1,6 +1,6 @@
 <?php //getting all the posts of account and account's friends that are in the same uni with him 
 	  //to add post_context
-		$sql="SELECT p.`post_id`, p.`post_question`, p.`post_val`, p.`post_tag`, p.`post_date` a.`username` FROM `posts`p 
+		$sql="SELECT p.`post_id`, p.`post_question`, p.`post_val`, p.`post_tag`, p.`post_date`, a.`username` FROM `posts`p 
         join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."'";
 		$xx = mysqli_query($con,$sql);
 	 
