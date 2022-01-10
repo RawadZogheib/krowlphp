8ca4093529d3557d8714d3d84565ef728802def6
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-10 16:19:05 +0200

    Update (Model)registInsert.inc.php

--- (Model)registInsert.inc.php
+++ (Model)registInsert.inc.php
@@ -1,9 +1,9 @@
 <?php 
 	$sql="INSERT INTO `account` (`account_Id`, `email`, `first_name`, `last_name`, `username`, `password`, `date_of_birth`, `photo`, `terms_of_service`, `crop_x`, `crop_y`, 
-									`crop_width`, `crop_height`, `university_ids`, `major_degree_ids`, `minor_degree_ids`, `vcode`,`chat_id`) 
+									`crop_width`, `crop_height`, `university_ids`, `major_degree_ids`, `minor_degree_ids`, `vcode`) 
 	VALUES (NULL, '".$email."', '".$first_name."', '".$last_name."', '".$username."', '".$hash."', '".$date_of_birth."', '".$photo."', '".$terms_of_service."', 
 									'".$crop_x."', '".$crop_y."','".$crop_width."', '".$crop_height."', '".$university_ids."', '".$major_degree_ids."', 
-									'".$minor_degree_ids."', '".$vCode."',0)";
+									'".$minor_degree_ids."', '".$vCode."')";
 									
 	$yy=mysqli_query($con,$sql);
 ?>
