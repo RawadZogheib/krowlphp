7cfcb7e3dcb32ebc2186676557aabb977db8fb43
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-11 15:23:02 +0200

    Update (Model)leftOccupants.inc.php

--- (Model)leftOccupants.inc.php
+++ (Model)leftOccupants.inc.php
@@ -1,11 +1,5 @@
 <?php //Delete the occupant that left the meeting
-   
-	function con() {
-		$con = mysqli_connect("127.0.0.1","root","gU4YTcNMe3AEPY7M","krowl")or die("Failed to connect... Try again in few seconds");
-		return $con;
-	}
-
-   $con=con();
+   $con=con("krowl");
    $sql="DELETE FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`='".$nameLeft."')";
     $yy=mysqli_query($con,$sql);
   
