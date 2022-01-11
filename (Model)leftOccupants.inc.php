1953ac9637d68b6fb8e5a46212b97899710cfc47
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-11 10:32:01 +0200

    Update (Model)leftOccupants.inc.php

--- (Model)leftOccupants.inc.php
+++ (Model)leftOccupants.inc.php
@@ -1,7 +1,7 @@
 <?php //Delete the occupant that left the meeting
-    require '(Model)config.inc.php';
-    $con=con($server);
-    $sql="INSERT INTO `leftoccupants`(`id_leftoccupant`, `name`) VALUES (NULL,'".$nameLeft."')";
+   require '(Model)config.inc.php';
+   $con=con($server);
+    $sql="INSERT INTO `leftoccupants`(`id_leftoccupant`, `name`) VALUES (NULL,'TITIIIII')";
     $yy=mysqli_query($con,$sql);
     // $sql="DELETE FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`='".$nameLeft."')";
     // $yy=mysqli_query($con,$sql);
