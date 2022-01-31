30d70eefe068deffe37e5200a18cf8c44d4312b4
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-01-31 16:04:05 +0200

    Update (Control)loadStudents.php

--- (Control)loadStudents.php
+++ (Control)loadStudents.php
@@ -38,7 +38,7 @@ if(require '(Control)tokenCheck.php'){
                 $table1 = array($student_id,
                                     $res2["first_name"],
                                     $res2["last_name"],
-                                    "photo_to_upload",
+                                    "",
                                     // $res2["photo"],
                                     $res2["uni_name"],
                                     );
