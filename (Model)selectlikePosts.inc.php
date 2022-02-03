ef87e3218e66c399cc8dc84a46996a50500a3149
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-03 16:34:25 +0200

    Update (Model)selectlikePosts.inc.php

--- (Model)selectlikePosts.inc.php
+++ (Model)selectlikePosts.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	//selecting likes of the post after inserting post_like_val in post_likes
 	  
-	$sql="SELECT `post_likes` FROM `posts` WHERE `account_Id` = '".$account_Id."' AND `post_id` = '".$post_id."'";
+	$sql="SELECT `post_likes` FROM `posts` WHERE `post_id` = '".$post_id."'";
 
 	$yy = mysqli_query($con,$sql);
 	 
