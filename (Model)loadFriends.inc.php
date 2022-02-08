d59e4dbc49aa38b8001a33b8ad90112a1f399d96
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-08 15:53:56 +0200

    Update (Model)loadFriends.inc.php

--- (Model)loadFriends.inc.php
+++ (Model)loadFriends.inc.php
@@ -1,5 +1,5 @@
 <?php 
-//getting the friends ids of the account that he has been talk to recently(recent==1)
+//getting the friends ids of the account (recent==0 || recent==1) to delete recent field later
 
 	$sql="SELECT  a.`account_Id`as account_Id,a.`username`as nameFriend, f.`friendship_id` as friend_id FROM `friendships`f join `account`a on f.id2=a.account_Id  WHERE `id1` ='".$account_Id."' AND `request`=2 
 	UNION SELECT  a.`account_Id`as account_Id,a.`username`as nameFriend,f.`friendship_id` as friend_id FROM `friendships`f join `account`a on f.id1=a.account_Id 
