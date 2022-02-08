6c61b350c79f1f9afa7d30ad21de0400921f34f5
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-08 14:12:38 +0200

    reply/post_val -> reply/post_likes

--- (Model)createPost.inc.php
+++ (Model)createPost.inc.php
@@ -2,7 +2,7 @@
 
     //Insert values into Posts
 
-    $sql="INSERT INTO `posts`(`post_id`, `account_Id`, `post_question`, `post_context`, `post_val`, `post_tag`) 
+    $sql="INSERT INTO `posts`(`post_id`, `account_Id`, `post_question`, `post_context`, `post_likes`, `post_tag`) 
     VALUES (NULL,'".$account_Id."','".$post_question."','".$post_context."',0,'".$post_tag."')";
 
     $yy=mysqli_query($con,$sql);
--- (Model)createReply.inc.php
+++ (Model)createReply.inc.php
@@ -2,7 +2,7 @@
 
     //Insert values into Replies
 
-    $sql="INSERT INTO `replies`(`reply_id`, `account_Id`, `post_id`, `reply_data`, `reply_val`) 
+    $sql="INSERT INTO `replies`(`reply_id`, `account_Id`, `post_id`, `reply_data`, `reply_likes`) 
     VALUES (NULL,'".$account_Id."','".$post_id."','".$reply_data."',0)";
 
     $yy=mysqli_query($con,$sql);
