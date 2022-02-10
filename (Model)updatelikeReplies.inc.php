3c739028f65faef4d56bb16b1634d4f2f03a7663
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-10 15:12:24 +0200

    Update (Model)updatelikeReplies.inc.php

--- (Model)updatelikeReplies.inc.php
+++ (Model)updatelikeReplies.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	//updating like of an existing row
 	  
-	$sql="UPDATE `reply_likes` SET `post_likes_val`='".$like_val."' WHERE `account_Id`='".$account_Id."' AND `reply_id` = '".$reply_id."'";
+	$sql="UPDATE `reply_likes` SET `reply_likes_val`='".$like_val."' WHERE `account_Id`='".$account_Id."' AND `reply_id` = '".$reply_id."'";
 
 	$xx = mysqli_query($con,$sql);
 	 
