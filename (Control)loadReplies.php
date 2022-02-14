5b84f8890d91b85443090d3028b4efa2c097ff9e
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-14 14:39:33 +0200

    jit +fixing loadReplies

--- (Control)loadReplies.php
+++ (Control)loadReplies.php
@@ -7,6 +7,7 @@ require '(Control)versionTest.php';
 if(require '(Control)tokenCheck.php'){
 
     $replies = array();
+    $tmp = array();
     $t1=0;
     
     if(!empty($data->account_Id) && !empty($data->post_id)){
@@ -16,17 +17,32 @@ if(require '(Control)tokenCheck.php'){
         
         require '(Model)loadReplies.inc.php';
         if(mysqli_num_rows($xx)>0){
-            $t1 = 1;
+            
             while($res = mysqli_fetch_assoc($xx)){	
-                array_push($replies,$res["reply_id"],
-                                    $res["username"],
-                                    $res["reply_data"],
-                                    $res["reply_likes"],
-                                    $res["reply_date"],
-                                    );
-            }	
-        }else{
-            $table_array[]=[];
+                $reply_id=$res["reply_id"];
+                array_push($tmp,$reply_id,
+                                $res["username"],
+                                $res["reply_data"],
+                                $res["reply_likes"],
+                                $res["reply_date"],
+                            );
+                // array_push($replies,$tmp);
+                // $tmp = array();
+                
+                require "(Model)checklikeReplies.inc.php"; //status of the button that has been pressed ( -1->unlike, 0->no button is pressed, 1->like )
+            if($res3["nbr"]==0){
+                $t1 = 1;
+                array_push($tmp,"0");
+            }else{
+                $t1 = 1;
+                array_push($tmp,$res3["reply_likes_val"]);
+            }
+            array_push($replies,$tmp);
+            $tmp = array();
+            }
+        }else if(mysqli_num_rows($xx) == 0){
+            $t1 = 2;
+            $replies = array();
         }
                     
             $json_array[0] = 'error4';
@@ -35,6 +51,8 @@ if(require '(Control)tokenCheck.php'){
 
             if($t1 == 1){
                 $json_array[0] = 'success';
+            }else if($t1 == 2){
+                $json_array[0] = 'empty';
             }
             echo json_encode($json_array);
 
--- jit/index.php
+++ jit/index.php
@@ -1,14 +1,62 @@
  <!DOCTYPE html> 
 	<?php //instead of account it was user 
-		if(!empty($_GET['table']) && !empty($_GET['account'])){
+		if(!empty($_GET['table']) && !empty($_GET['account']) && !empty($_GET['type'])){
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table']).'"';
       $account = htmlspecialchars("'".$_GET["account"]."'");
-      
+      $type = htmlspecialchars("'".$_GET["type"]."'");
 		}
     else{
       echo 'account id not found';
     }
 	?>
+  <?php if($type == "silent"){ ?>
+    <html>
+      <head>
+        <script src='https://8x8.vc/external_api.js' async></script>
+        <style>html, body, #jaas-container { height: 100%; }</style>
+        <script type="text/javascript">
+
+          const domain = '8x8.vc';
+          window.onload = () => {
+            var api = new JitsiMeetExternalAPI(domain, {
+              roomName: <?php echo $room;?>,
+              parentNode: document.querySelector('#jaas-container'),
+
+              configOverwrite: {
+                  // disable the prejoin page
+                  prejoinPageEnabled: false,
+                  
+                  //optionally we can control the mute state on join from the emebedding application
+                  startWithAudioMuted: [true],
+                  startWithVideoMuted: [true],
+                  startSilent: true,
+                  disableInitialGUM: false,
+                },
+                // optionally, we can have the meeting select the devices we want
+                devices: {
+                  audioInput: '<deviceLabel>',
+                  audioOutput: '<deviceLabel>',
+                  videoInput: '<deviceLabel>'
+                },
+                interfaceConfigOverwrite: { SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
+                userInfo: {
+                displayName:<?php echo $account?>
+                },
+            });
+            api.addListener('readyToClose',window.close(););
+            // 
+            // require '(Model)config.inc.php';
+            //   $con=con("krowl");
+            //   $sql="DELETE FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`='TOTOOO')";
+            //     $yy=mysqli_query($con,$sql); 
+            
+          }
+          
+        </script>
+      </head>
+      <body><div id="jaas-container" ></div></body>
+    </html>
+  <?php }else{ ?>
     <html>
       <head>
         <script src='https://8x8.vc/external_api.js' async></script>
@@ -53,3 +101,4 @@
       </head>
       <body><div id="jaas-container" ></div></body>
     </html>
+    <?php } ?>
