89911d1128ebf3d3a443406e5545e1c66eea522e
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-15 16:05:19 +0200

    Update (Model)countReplies.inc.php

--- (Model)countReplies.inc.php
+++ (Model)countReplies.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-	$sql="SELECT count(*) as nbr FROM `replies` WHERE `post_id`= '".$post_id.""; //where univeristy_ids are not null
+	$sql="SELECT count(*) as nbr FROM `replies` WHERE `post_id`= '".$post_id."'"; //where univeristy_ids are not null
 
 	$zz1 = mysqli_query($con,$sql);
 	$res2 = mysqli_fetch_assoc($zz1);
