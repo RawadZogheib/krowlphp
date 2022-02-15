ba720ed4835e457579387b5163e567e22488de26
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-15 15:21:28 +0200

    Update (Model)loadStudents1.inc.php

--- (Model)loadStudents1.inc.php
+++ (Model)loadStudents1.inc.php
@@ -7,7 +7,7 @@
 	$start=$stable*($current-1);
 	$end=$stable*$current;
 
-	$sql= "SELECT a.`account_Id`,a.`first_name`,a.`last_name`,a.`photo`,u.`uni_name` FROM `account`a JOIN `universities`u 
+	$sql= "SELECT a.`account_Id`,a.`first_name`,a.`last_name`,a.`photo`,a.`bio`,u.`uni_name` FROM `account`a JOIN `universities`u 
 	on a.`university_ids`=u.`uni_id` WHERE a.`account_Id`!='".$account_Id."' ORDER BY a.first_name ASC LIMIT ".$start.",".$end."";
 
 	$xx = mysqli_query($con,$sql);
