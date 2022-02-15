a3c2dfc915c7871fae1b056d5f902d1265296d64
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-15 12:53:53 +0200

    Update (Model)getNewReplyId.inc.php

--- (Model)getNewReplyId.inc.php
+++ (Model)getNewReplyId.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
     $sql="SELECT `reply_id` FROM `replies` WHERE `account_Id`='".$account_Id."' AND `post_id`='".$post_id."' 
-    AND `reply_data`='".$reply_data."' AND `reply_date`='".$reply_date."'";
+    AND `reply_data`='".$reply_data."' AND `reply_date`=".$reply_date."";
 
     $zz=mysqli_query($con,$sql);
 ?>
