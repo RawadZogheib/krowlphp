f493606a1e0c121f26f69fbfc0d21877b30d37c4
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-15 11:51:52 +0200

    Update index.php

--- jit/index.php
+++ jit/index.php
@@ -4,7 +4,6 @@
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table']).'"';
       $account = htmlspecialchars("'".$_GET["account"]."'");
       $type = htmlspecialchars("".$_GET["type"]."");
-      echo $type;
 		}
     else{
       echo 'account id not found';
@@ -30,6 +29,7 @@
                   //optionally we can control the mute state on join from the emebedding application
                   startWithAudioMuted: [true],
                   startWithVideoMuted: [true],
+                  disableInitialGUM: false,
                   toolbarButtons: ['camera','chat','desktop','download','embedmeeting','etherpad','feedback','filmstrip','fullscreen','hangup','help','invite','livestreaming','participants-pane','profile','raisehand','recording','security','select-background','settings','sharedvideo','shortcuts','stats','tileview','toggle-camera','videoquality','__end']
 
                 },
