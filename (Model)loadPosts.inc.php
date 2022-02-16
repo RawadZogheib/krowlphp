04586a58623b8f3cca0ab8956d02988b1ab9e118
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-16 10:55:48 +0200

    Update (Model)loadPosts.inc.php

--- (Model)loadPosts.inc.php
+++ (Model)loadPosts.inc.php
@@ -2,7 +2,7 @@
 	//Getting all the posts of account and account's friends that are in the same uni with him 
 	  
 	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date`, a.`username` FROM `posts`p 
-	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."' ORDER BY r.`post_date` DESC";
+	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."' ORDER BY p.`post_date` DESC";
 
 	$xx = mysqli_query($con,$sql);
 	 
