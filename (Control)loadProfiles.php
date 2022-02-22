7ee8b8f6468ca7648387d53fd07a689fa1887c7b
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-22 11:33:16 +0200

    Update (Control)loadProfiles.php

--- (Control)loadProfiles.php
+++ (Control)loadProfiles.php
@@ -68,11 +68,11 @@ if(require '(Control)tokenCheck.php'){
 
                     while($res2 = mysqli_fetch_assoc($xx2)){	
                         array_push($tmp,$res2["post_id"],
-                                        // $res2["post_tag"],
-                                        // $res2["post_question"],
-                                        // $res2["post_likes"],
-                                        // $res2["post_date"],
-                                        // $res2["post_context"],
+                                        $res2["post_tag"],
+                                        $res2["post_question"],
+                                        $res2["post_likes"],
+                                        $res2["post_date"],
+                                        $res2["post_context"],
                                     );   
                         array_push($posts,$tmp);
                         $tmp=array();                    
@@ -92,9 +92,9 @@ if(require '(Control)tokenCheck.php'){
                         while($res3 = mysqli_fetch_assoc($xx3)){	
                             array_push($tmp,$res3["post_id"],
                                             $res3["reply_id"],
-                                            // $res3["reply_data"],
-                                            // $res3["reply_likes"],
-                                            // $res3["reply_date"]
+                                            $res3["reply_data"],
+                                            $res3["reply_likes"],
+                                            $res3["reply_date"]
                                         );   
                             array_push($replies,$tmp);
                             $tmp=array();                    
