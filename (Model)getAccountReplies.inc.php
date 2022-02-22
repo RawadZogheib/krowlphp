b94b4d34a4b367003eca48c93943d674c0885bdb
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-22 10:33:19 +0200

    update loadProfiles
    removing extra data from post and replies, and letting post_id in body[2], and post_id, reply_id in body[3]

--- (Model)getAccountReplies.inc.php
+++ (Model)getAccountReplies.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	//Getting all the replies of a post
 	  
-	$sql="SELECT r.`reply_id`,r.`reply_data`, r.`reply_likes`, r.`reply_date` FROM `replies`r
+	$sql="SELECT r.`reply_id`,r.`post_id`,r.`reply_data`, r.`reply_likes`, r.`reply_date` FROM `replies`r
 	 WHERE r.account_Id='".$account_Id."'";
 
 	$xx3 = mysqli_query($con,$sql);
