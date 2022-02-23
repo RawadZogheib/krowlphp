e96e376f759f1ea669e47fffbe9c67c02da4415a
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-23 11:57:07 +0200

    fff

--- (Model)getAccountPosts.inc.php
+++ (Model)getAccountPosts.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	//Getting all the posts of account and account's friends that are in the same uni with him 
 	  
-	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date` FROM `posts`p 
+	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date`,a.`first_name`,a.`last_name` FROM `posts`p 
 	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.account_Id='".$account_Id."'ORDER BY p.`post_date` DESC";
 
 	$xx2 = mysqli_query($con,$sql);
