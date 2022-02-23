d651419e3426def1660c14d8d58550377e663069
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-23 10:00:20 +0200

    thry

--- (Model)getAccountPosts.inc.php
+++ (Model)getAccountPosts.inc.php
@@ -2,7 +2,7 @@
 	//Getting all the posts of account and account's friends that are in the same uni with him 
 	  
 	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date` FROM `posts`p 
-	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.account_Id='".$account_Id."'";
+	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.account_Id='".$account_Id."'ORDER BY p.`post_date` DESC";
 
 	$xx2 = mysqli_query($con,$sql);
 	 
--- (Model)getAccountReplies.inc.php
+++ (Model)getAccountReplies.inc.php
@@ -2,7 +2,7 @@
 	//Getting all the replies of a post
 	  
 	$sql="SELECT r.`reply_id`,r.`reply_data`, r.`reply_likes`, r.`reply_date`,p.`post_question`,p.`post_tag`,p.`post_likes`,p.`post_context`,r.`post_id`,p.`post_date`
-	FROM `replies`r LEFT JOIN `posts`p on r.`post_id`=p.`post_id` WHERE r.account_Id='".$account_Id."'";
+	FROM `replies`r LEFT JOIN `posts`p on r.`post_id`=p.`post_id` WHERE r.account_Id='".$account_Id."'ORDER BY r.`reply_date` DESC";
 
 	$xx3 = mysqli_query($con,$sql);
 	 
