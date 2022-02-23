83c40a2d45a01b041291cc495bfb249c312a9101
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-02-23 12:24:10 +0200

    first&last name -> username

--- (Model)getAccountReplies.inc.php
+++ (Model)getAccountReplies.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	//Getting all the replies of a post
 	  
-	$sql="SELECT r.`reply_id`,r.`reply_data`, r.`reply_likes`, r.`reply_date`,p.`post_question`,p.`post_tag`,p.`post_likes`,p.`post_context`,r.`post_id`,p.`post_date`,a.`first_name`,a.`last_name` 
+	$sql="SELECT r.`reply_id`,r.`reply_data`, r.`reply_likes`, r.`reply_date`,p.`post_question`,p.`post_tag`,p.`post_likes`,p.`post_context`,r.`post_id`,p.`post_date`,a.`first_name`,a.`last_name`,a.`username`
 	FROM `replies`r LEFT JOIN `posts`p on r.`post_id`=p.`post_id` LEFT JOIN `account`a on p.`account_Id`=a.`account_Id` WHERE r.account_Id='".$account_Id."'ORDER BY r.`reply_date` DESC";
 
 	$xx3 = mysqli_query($con,$sql);
