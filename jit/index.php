a96beeeefa7e510c81c09cdcaebc414378e70f96
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-02 16:49:05 +0200

    Update index.php

--- jit/index.php
+++ jit/index.php
@@ -81,7 +81,7 @@
                 },
                 interfaceConfigOverwrite: { 
                 APP_NAME: 'Krowl',  
-                DEFAULT_BACKGROUND: './jit/krowl_logo2.png',
+                DEFAULT_BACKGROUND: '#33A5FF',
                 DEFAULT_LOGO_URL: './jit/krowl_logo2.png',
                 SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
                 userInfo: {
