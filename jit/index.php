ebf0a1a98746ffc7d09e04a05c1dd22da155586e
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-07 11:38:00 +0200

    sss

--- jit/index.php
+++ jit/index.php
@@ -1,106 +1,15 @@
  <!DOCTYPE html> 
 	<?php //instead of account it was user 
-    // Target URL
-$url = "https://KwikCode.net/krowlphp/jit/index.php";
-  
-// Fetching headers
-$headers = get_headers($url);
-  
-// Printing headers
-print_r($headers);
-    echo '1111111112221';
-   
+    
 		if(!empty($_GET['table_name']) && !empty($_GET['account']) && !empty($_GET['type'])){
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table_name']).'"';
       $account = htmlspecialchars("'".$_GET["account"]."'");
       $type = htmlspecialchars("".$_GET["type"]."");
+      header("Location:index1.php");
+      exit();
 		}
     else{
       echo 'account id not found';
     }
 	?>
-  <?php if($type == "silent"){ ?>
-    <html>
-      <head>
-        <script src='https://8x8.vc/external_api.js' async></script>
-        <style>html, body, #jaas-container { height: 100%; }</style>
-        <script type="text/javascript">
-
-          const domain = '8x8.vc';
-          window.onload = () => {
-            var api = new JitsiMeetExternalAPI(domain, {
-              roomName: <?php echo $room;?>,
-              parentNode: document.querySelector('#jaas-container'),
-
-              configOverwrite: {
-                  // disable the prejoin page
-                  prejoinPageEnabled: false,
-                  
-                  //optionally we can control the mute state on join from the emebedding application
-                  startWithAudioMuted: [true],
-                  startWithVideoMuted: [true],
-                  disableInitialGUM: true,
-                  toolbarButtons: ['camera','chat','desktop','download','embedmeeting','etherpad','feedback','filmstrip','fullscreen','hangup','help','invite','livestreaming','participants-pane','profile','raisehand','recording','security','select-background','settings','sharedvideo','shortcuts','stats','tileview','toggle-camera','videoquality','__end']
-
-                },
-                // optionally, we can have the meeting select the devices we want
-                devices: {
-                  audioInput: '<deviceLabel>',
-                  audioOutput: '<deviceLabel>',
-                  videoInput: '<deviceLabel>'
-                },
-                interfaceConfigOverwrite: { SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
-                userInfo: {
-                displayName:<?php echo $account?>
-                },
-            });
-            
-          }
-          
-        </script>
-      </head>
-      <body><div id="jaas-container" ></div></body>
-    </html>
-  <?php }else{ ?>
-    <html>
-      <head>
-        <script src='https://8x8.vc/external_api.js' async></script>
-        <style>html, body, #jaas-container { height: 100%; }</style>
-        <script type="text/javascript">
-
-          const domain = '8x8.vc';
-          window.onload = () => {
-            var api = new JitsiMeetExternalAPI(domain, {
-              roomName: <?php echo $room;?>,
-              parentNode: document.querySelector('#jaas-container'),
-
-              configOverwrite: {
-                  // disable the prejoin page
-                  prejoinPageEnabled: false,
-                  
-                  //optionally we can control the mute state on join from the emebedding application
-                  startWithAudioMuted: [true],
-                  startWithVideoMuted: [true],
-                  disableInitialGUM: true,
-                },
-                // optionally, we can have the meeting select the devices we want
-                devices: {
-                  audioInput: '<deviceLabel>',
-                  audioOutput: '<deviceLabel>',
-                  videoInput: '<deviceLabel>'
-                },
-                interfaceConfigOverwrite: { 
-                APP_NAME: 'Krowl',  
-                SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
-                userInfo: {
-                displayName:<?php echo $account?>
-                },
-            });
-            
-          }
-          
-        </script>
-      </head>
-      <body><div id="jaas-container" ></div></body>
-    </html>
-    <?php } ?>
+  
---
+++ jit/index1.php
@@ -0,0 +1,85 @@
+<?php if($type == "silent"){ ?>
+    <html>
+      <head>
+        <script src='https://8x8.vc/external_api.js' async></script>
+        <style>html, body, #jaas-container { height: 100%; }</style>
+        <script type="text/javascript">
+
+          const domain = '8x8.vc';
+          window.onload = () => {
+            var api = new JitsiMeetExternalAPI(domain, {
+              roomName: <?php echo $room;?>,
+              parentNode: document.querySelector('#jaas-container'),
+
+              configOverwrite: {
+                  // disable the prejoin page
+                  prejoinPageEnabled: false,
+                  
+                  //optionally we can control the mute state on join from the emebedding application
+                  startWithAudioMuted: [true],
+                  startWithVideoMuted: [true],
+                  disableInitialGUM: true,
+                  toolbarButtons: ['camera','chat','desktop','download','embedmeeting','etherpad','feedback','filmstrip','fullscreen','hangup','help','invite','livestreaming','participants-pane','profile','raisehand','recording','security','select-background','settings','sharedvideo','shortcuts','stats','tileview','toggle-camera','videoquality','__end']
+
+                },
+                // optionally, we can have the meeting select the devices we want
+                devices: {
+                  audioInput: '<deviceLabel>',
+                  audioOutput: '<deviceLabel>',
+                  videoInput: '<deviceLabel>'
+                },
+                interfaceConfigOverwrite: { SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
+                userInfo: {
+                displayName:<?php echo $account?>
+                },
+            });
+            
+          }
+          
+        </script>
+      </head>
+      <body><div id="jaas-container" ></div></body>
+    </html>
+  <?php }else{ ?>
+    <html>
+      <head>
+        <script src='https://8x8.vc/external_api.js' async></script>
+        <style>html, body, #jaas-container { height: 100%; }</style>
+        <script type="text/javascript">
+
+          const domain = '8x8.vc';
+          window.onload = () => {
+            var api = new JitsiMeetExternalAPI(domain, {
+              roomName: <?php echo $room;?>,
+              parentNode: document.querySelector('#jaas-container'),
+
+              configOverwrite: {
+                  // disable the prejoin page
+                  prejoinPageEnabled: false,
+                  
+                  //optionally we can control the mute state on join from the emebedding application
+                  startWithAudioMuted: [true],
+                  startWithVideoMuted: [true],
+                  disableInitialGUM: true,
+                },
+                // optionally, we can have the meeting select the devices we want
+                devices: {
+                  audioInput: '<deviceLabel>',
+                  audioOutput: '<deviceLabel>',
+                  videoInput: '<deviceLabel>'
+                },
+                interfaceConfigOverwrite: { 
+                APP_NAME: 'Krowl',  
+                SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
+                userInfo: {
+                displayName:<?php echo $account?>
+                },
+            });
+            
+          }
+          
+        </script>
+      </head>
+      <body><div id="jaas-container" ></div></body>
+    </html>
+    <?php } ?>
