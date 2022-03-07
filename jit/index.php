4bd7f8f6fcb3566747772b7aa9aa032994965149
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-07 11:54:22 +0200

    Update index.php

--- jit/index.php
+++ jit/index.php
@@ -5,8 +5,9 @@
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table_name']).'"';
       $account = htmlspecialchars("'".$_GET["account"]."'");
       $type = htmlspecialchars("".$_GET["type"]."");
-      header("Location:index1.php");
       echo $type;
+      header("Location:index1.php");
+
       exit();
 		}
     else{
