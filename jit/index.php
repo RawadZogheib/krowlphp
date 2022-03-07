ae267f4b17c152e1415e5b34410fcf593caed673
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-07 12:47:18 +0200

    Update index.php

--- jit/index.php
+++ jit/index.php
@@ -4,11 +4,11 @@
 		if(!empty($_GET['table_name']) && !empty($_GET['account']) && !empty($_GET['type'])){
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table_name']).'"';
       $account = htmlspecialchars("'".$_GET["account"]."'");
-      $type = htmlspecialchars("".$_GET["type"]."");?>
-      <form method="post" action="index1.php">
-        <input value='DATAFLOW' name="name">
-        <input type="silentoookdd" name="type">
-      </form>
+      $type = htmlspecialchars("".$_GET["type"]."");
+      $_POST["name"]=$room;
+      $_POST["type"]=$type;
+      
+      ?>
      <?php header("Location:index1.php");
 
       exit();
