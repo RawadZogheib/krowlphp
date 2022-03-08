363670dab9c4961d4a61402e46304a0d442f8840
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-08 16:17:55 +0200

    Update (Model)loadParticipants.inc.php

--- (Model)loadParticipants.inc.php
+++ (Model)loadParticipants.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-	$sql= "SELECT a.`account_Id`, a.`username`,a.`photo FROM `participants`p JOIN `account`a ON p.`account_Id`=a.`account_Id` WHERE o.`table_id` =$table_id";
+	$sql= "SELECT a.`account_Id`, a.`username`,a.`photo FROM `participants`p JOIN `account`a ON p.`account_Id`=a.`account_Id` WHERE p.`table_id` ='".$table_id."'";
 
 	$yy1 = mysqli_query($con,$sql);
 	
