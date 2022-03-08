c0f70d80aaffa74ee72ec4f27b73ee4262ca4a4e
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-08 17:02:43 +0200

    Update (Model)countPrivateTables.inc.php

--- (Model)countPrivateTables.inc.php
+++ (Model)countPrivateTables.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-	$sql="SELECT count(*)as nbr FROM `tables` WHERE `table_uni` = '".$user_uni."' AND `isPrivate` = '".$isPrivate."'AND `account_Id`= '".$account_Id."'";
+	$sql="SELECT count(*)as nbr FROM `particpant` WHERE account_Id`= '".$account_Id."'";
 
 	$xx = mysqli_query($con,$sql);
 	$res = mysqli_fetch_assoc($xx);
