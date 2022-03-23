87e3fd44a5fa633ce91ad5e7602dba10a523f622
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-03-23 10:23:28 +0200

    Update (Model)checkisUserAvailable.inc.php

--- (Model)checkisUserAvailable.inc.php
+++ (Model)checkisUserAvailable.inc.php
@@ -2,7 +2,7 @@
 
 	//Check if the the user has already take place in a table and want to join a second place
 
-	$sql="SELECT count(*)as nbr FROM `occupants` WHERE `account_Id` = '".$account_Id."' UNION SELECT count(*)as nbr FROM `participants` WHERE `account_Id` = '".$account_Id."' ";
+	$sql="SELECT count(*)as nbr FROM `occupants` WHERE `account_Id` = '".$account_Id."'";
 	
 	$xx = mysqli_query($con,$sql);
 	$res6 = mysqli_fetch_assoc($xx);
