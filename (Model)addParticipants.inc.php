97ad0ed682b6256b84bf45a4adf903c7f3962826
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-07 17:23:13 +0300

    Update (Model)addParticipants.inc.php

--- (Model)addParticipants.inc.php
+++ (Model)addParticipants.inc.php
@@ -1,8 +1,12 @@
 <?php 
 
-	$sql= "INSERT INTO `participant`(`particpant_id`, `table_id`, `account_Id`) VALUES (NULL,'".$table_id."','".$friend_id."')";
-
+	//$sql= "INSERT INTO `participant`(`particpant_id`, `table_id`, `account_Id`) VALUES (NULL,'".$table_id."','".$friend_id."')";
+	$sql="INSERT INTO participant (table_id, account_Id)
+	SELECT * FROM (SELECT '".$table_id."', '".$friend_id."') AS tmp
+	WHERE NOT EXISTS (
+		SELECT table_id,account_Id FROM participant WHERE table_id =  '".$table_id."' AND account_Id='".$friend_id."') LIMIT 1";
 	$zz = mysqli_query($con,$sql);
 	
+
 
 ?>
