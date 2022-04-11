db9ba3d6dd059baf07e9355a141ee63caecec5f7
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-11 13:27:17 +0300

    Update (Model)loadPrivateTables.inc.php

--- (Model)loadPrivateTables.inc.php
+++ (Model)loadPrivateTables.inc.php
@@ -7,7 +7,7 @@
 	$start=$stable*($current-1);
 	$end=$stable*$current;
 
-	$sql= "SELECT * FROM `tables`t JOIN `participant`p ON t.`table_id`=p.`table_id` WHERE t.`table_uni`='".$user_uni."' AND t.`isPrivate`='".$isPrivate."' AND p. `account_Id`= '".$account_Id."' ORDER BY created_at DESC LIMIT ".$start.",".$end."";
+	$sql= "SELECT * FROM `tables`t JOIN `participant`p ON t.`table_id`=p.`table_id` WHERE t.`table_uni`='".$user_uni."' AND t.`isPrivate`='".$isPrivate."' AND p.`account_Id`= '".$account_Id."' ORDER BY created_at DESC LIMIT ".$start.",".$end."";
 
 	$xx = mysqli_query($con,$sql);
 	
