fa72c35957474a4da22b85d1bf7506d80eeda93b
krowl <askot300@gmail.com>
2022-04-14 10:12:30 +0300

    Update (Control)versionTest.php

--- (Control)versionTest.php
+++ (Control)versionTest.php
@@ -2,7 +2,7 @@
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 // Token configuration
-$server="krowl";// DB name
+$server="krowltest";// DB name
 $time = 1;// Token expire time
 $tokenLimitation = 5;// Token per account
 
