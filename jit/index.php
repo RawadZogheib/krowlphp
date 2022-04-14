cc75bee8ce14b061e69f0ac6c796195376746a9a
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-14 15:08:45 +0300

    Update index.php

--- jit/index.php
+++ jit/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html> 
 	<?php //instead of account it was user 
-		if(!empty($_GET['table']) || !empty($_GET['account'])){
+		if(!empty($_GET['table']) && !empty($_GET['account'])){
 			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table']).'"';
       //$account = htmlspecialchars("'".$_GET["account"]."'");
       $account ="1";
