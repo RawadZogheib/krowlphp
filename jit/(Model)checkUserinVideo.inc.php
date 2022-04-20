7aff502eec0cf49a4b71ff79d7620b02a4ebe43d
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-20 15:29:52 +0300

    Update (Model)checkUserinVideo.inc.php

--- jit/(Model)checkUserinVideo.inc.php
+++ jit/(Model)checkUserinVideo.inc.php
@@ -3,7 +3,7 @@
     //Insert values into Replies
     require '../(Model)config.inc.php';
     $con=con("krowltest");
-    $sql="SELECT count(*)as nbr,`occupant_id` FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`=$account) AND `occupant_video`=0";
+    $sql="SELECT count(*)as nbr,`occupant_id` FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`=".$account.") AND `occupant_video`=0";
 
     $zz=mysqli_query($con,$sql);
     $res1= mysqli_fetch_assoc($zz);
