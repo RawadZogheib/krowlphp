f0dc36c7db966a2e90746c29f1d29f4bd00a6dce
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-20 12:41:09 +0300

    Update (Model)insertPosition.inc.php

--- (Model)insertPosition.inc.php
+++ (Model)insertPosition.inc.php
@@ -2,8 +2,8 @@
 
     //Insert into Occupants
 
-    $sql="INSERT INTO `occupants`(`occupant_id`, `table_id`, `account_Id`, `inviter_id`, `position`)
-    VALUES (NULL,'".$table_id."','".$account_Id."',0,'".$position."')";
+    $sql="INSERT INTO `occupants`(`occupant_id`, `table_id`, `account_Id`, `inviter_id`, `position`,`occupant_video`)
+    VALUES (NULL,'".$table_id."','".$account_Id."',0,'".$position."',0)";
 
     $xx = mysqli_query($con,$sql);
 
