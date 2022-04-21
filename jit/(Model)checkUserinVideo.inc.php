611dfa51cf05b6c2c8175245974dbb403d5f5b9c
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-21 16:27:06 +0300

    Update (Model)checkUserinVideo.inc.php

--- jit/(Model)checkUserinVideo.inc.php
+++ jit/(Model)checkUserinVideo.inc.php
@@ -1,14 +1,14 @@
 <?php 
 
-    //Insert values into Replies
+    //occupant_video = 0 -> he's sitting on a table but didn't join the jit meeting 
     require '../(Model)config.inc.php';
     $con=con("krowltest");
+
     $sql="SELECT count(*)as nbr,`occupant_id` FROM `occupants` WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`=".$account.") AND `occupant_video`=0";
 
     $zz=mysqli_query($con,$sql);
     $res1= mysqli_fetch_assoc($zz);
-    echo "AAAAAAAAA";
-    echo $res1;
+
 
 ?>
 
