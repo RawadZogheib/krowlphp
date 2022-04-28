58f1123c133de9ab5f70b1246480aef4ce846abb
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-28 14:18:09 +0300

    h

--- jit/index1.php
+++ jit/index1.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html> 
-	<?php //instead of account it was user 
-		if(!empty($_GET['table']) && !empty($_GET['account'])){
-			$room = '"vpaas-magic-cookie-5bea10f9861f4c588b1c164f2f3113de/'.htmlspecialchars($_GET['table']).'"';
-      $account = '"'.htmlspecialchars($_GET['account']).'"';
-    
-    require "(Model)checkUserinVideo.inc.php";
-      if($res1["nbr"]==1){
-      require "(Model)insertUserinVideo.inc.php";
-      if($xx3){
-      require "(Model)getTypeTable.inc.php";
-      if(mysqli_num_rows($yy)>0){
-        $res = mysqli_fetch_assoc($yy);
-        $type=$res["isSilent"];
-      }
- if($type == "2"){ ?>
-    <html>
+<html>
       <head>
         <script src='https://8x8.vc/external_api.js' async></script>
-        <style>html, body, #jaas-container { height: 100%; }</style>
+        <style>html, body, #jaas-container { height: 99.5%; }</style>
         <script type="text/javascript">
 
           const domain = '8x8.vc';
@@ -34,8 +18,7 @@
                   startWithAudioMuted: [true],
                   startWithVideoMuted: [true],
                   disableInitialGUM: true,
-                  toolbarButtons: ['camera','chat','desktop','download','embedmeeting','etherpad','feedback','filmstrip','fullscreen','help','livestreaming','participants-pane','profile','raisehand','recording','security','settings','sharedvideo','shortcuts','stats','tileview','toggle-camera','videoquality']
-
+                   toolbarButtons: ['camera','chat','closedcaptions','desktop','download','embedmeeting','etherpad','feedback','filmstrip','fullscreen','help','livestreaming','microphone','participants-pane','profile','raisehand','recording','security','settings','shareaudio','sharedvideo','shortcuts','tileview','toggle-camera','videoquality']
                 },
                 // optionally, we can have the meeting select the devices we want
                 devices: {
@@ -55,12 +38,3 @@
       </head>
       <body><div id="jaas-container" ></div></body>
     </html>
-  <?php }else{ require 'index1.php';?>
-   
-    <?php }}
-    }else{
-      echo 'Error';
-    }
-    }else{
-      echo 'Field cannot be empty.';
-        } ?>
