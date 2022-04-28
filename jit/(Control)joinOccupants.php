04cc20ae1c8b79256d3813f1c88f6d38e74c0c90
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-04-28 16:45:19 +0300

    Update (Control)joinOccupants.php

--- jit/(Control)joinOccupants.php
+++ jit/(Control)joinOccupants.php
@@ -6,10 +6,10 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 if(!empty($data->data->name)){ //getting the name of the participant who left
   $nameJoined = htmlspecialchars($data->data->name);
-  require '(Model)joinOccupants.inc.php'; //delete the particpant from occupants so the position will be free again
+ 
 }
 else{
     $nameJoined="failed";
 }
-
+require '(Model)joinOccupants.inc.php'; //delete the particpant from occupants so the position will be free again
 ?>
