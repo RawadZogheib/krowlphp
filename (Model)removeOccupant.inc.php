69ea1fee5f56209736e25811b372024f908b22cb
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-06 10:05:08 +0300

    Update (Model)removeOccupant.inc.php

--- (Model)removeOccupant.inc.php
+++ (Model)removeOccupant.inc.php
@@ -1,6 +1,5 @@
 <?php //Delete the occupant that left the meeting
-	require '(Model)config.inc.php';
-   $con=con("krowl");
+
    $sql="DELETE FROM `occupants` WHERE `account_Id`='".$account_Id."' AND `table_id`='".$table_id."'";
     $yy=mysqli_query($con,$sql);
   
