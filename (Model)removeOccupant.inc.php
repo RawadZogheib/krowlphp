b11cf13ed35ee9ff87280e2bd32f4ada333cb150
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-06 10:16:18 +0300

    Update (Model)removeOccupant.inc.php

--- (Model)removeOccupant.inc.php
+++ (Model)removeOccupant.inc.php
@@ -3,10 +3,5 @@
    $sql="DELETE FROM `occupants` WHERE `account_Id`='".$account_Id."' AND `table_id`='".$table_id."'";
     $yy=mysqli_query($con,$sql);
   
-    //header("Location: http://www.google.com/");
-    //echo "<script>window.close();</script>";
+ 
 ?>
-
-<!-- <script type="text/javascript">
-    window.close();
-</script> -->
