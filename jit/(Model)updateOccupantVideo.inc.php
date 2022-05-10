e20b985e404e62ec1d33afae3b40d8a6a7851bf3
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-10 16:14:20 +0300

    Update (Model)updateOccupantVideo.inc.php

--- jit/(Model)updateOccupantVideo.inc.php
+++ jit/(Model)updateOccupantVideo.inc.php
@@ -2,7 +2,7 @@
 	//updating occupant_video of an existing row
 	  
 	
-	$sql="UPDATE `occupants` SET `occupant_video`= '".$occupant_video."' AND `load_jit`= 0 WHERE `occupant_id`='".$occupant_id."'";
+	$sql="UPDATE `occupants` SET `occupant_video`= '".$occupant_video."',`load_jit`= 0 WHERE `occupant_id`='".$occupant_id."'";
 
 	$xx3 = mysqli_query($con,$sql);
 	 
