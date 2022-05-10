5de406efa4bd5ffaaf33012702d73e953c06c826
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-10 16:06:26 +0300

    s

--- jit/(Model)updateLoadJit.inc.php
+++ jit/(Model)updateLoadJit.inc.php
@@ -2,7 +2,7 @@
 	//updating occupant_video of an existing row
 	  
 	
-	$sql="UPDATE `occupants` SET `load_jit`= '".$load_jit."' WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`=".$account.")";
+	$sql="UPDATE `occupants` SET `load_jit`= '1' WHERE `account_Id`=(SELECT `account_Id` FROM `account` WHERE `username`=".$account.")";
 
 	$xx3 = mysqli_query($con,$sql);
 	 
