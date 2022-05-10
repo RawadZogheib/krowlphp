ecec26c6597c215aeac6c5b0e4621129a4bf80f8
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-10 12:04:05 +0300

    Update (Model)removeParticipant.inc.php

--- (Model)removeParticipant.inc.php
+++ (Model)removeParticipant.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	  
-	$sql="DELETE FROM `participant`p JOIN `tables`t on p.`table_id`=t.`table_id` WHERE
+	$sql="DELETE p FROM `participant`p JOIN `tables`t on p.`table_id`=t.`table_id` WHERE
      p.`table_id` = '".$table_id."' AND p.`account_Id` = '".$account_Id."' AND t.`admin_id`='".$admin_id."'";
 
 	$yy = mysqli_query($con,$sql);
