0cf6bf6f8498899746d0a7f9f81f6d21a6ffa662
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-19 13:12:33 +0300

    Update (Control)versionTest.php

--- (Control)versionTest.php
+++ (Control)versionTest.php
@@ -2,16 +2,11 @@
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
- //version
-$server = "krowlTest";// DB name
-$time = 1;// Token expire time
-$tokenLimitation = 5;// Token per account  
 
-//require 'Global/globalVariables.php';
 if(!empty($data->version)){
 
     $version = htmlspecialchars($data->version);
-    $apiVersion = "v1.0";
+    require 'Global/globalVariables.php';
 
     if($version != $apiVersion){
         require '(View)ErrorVersion.php';
