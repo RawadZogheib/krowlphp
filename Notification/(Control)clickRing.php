2629fe0a1d50fbe8c0780501fd7ffd4d740db103
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-05-26 12:20:47 +0300

    Update (Control)clickRing.php

--- Notification/(Control)clickRing.php
+++ Notification/(Control)clickRing.php
@@ -1,12 +1,15 @@
 <?php 
   
     $receiver_id = $account_Id;
-    $json_array[0] = 'error4';
 
     $notif_nbr= 0;
     require '(Model)updateNotifNbr.inc.php';
     if(mysqli_affected_rows($con)>0){
+
         $json_array[0] = 'success';
+
+    }else if(mysqli_affected_rows($con) == -1){
+        $json_array[0] = 'error4';
     }
 
 ?>
