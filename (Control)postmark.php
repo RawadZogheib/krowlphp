ece1b78212c4c464679b05ff0798bebd194e08f8
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-06-13 15:23:39 +0300

    Update (Control)postmark.php

--- (Control)postmark.php
+++ (Control)postmark.php
@@ -1,16 +1,49 @@
 <?php 
+// require_once('.\vendor\autoload.php');
+// use Postmark\PostmarkClient;
+
+// // Example request
+// $client = new PostmarkClient("dd966b63-c58c-4772-8123-05e4e98acd1a");
+
+// $sendResult = $client->sendEmail(
+//   "team@krowl.io",
+//    $email,
+//   "Hello ".$username ." from Postmark!",
+//   "This is just a friendly 'hello' from your friends at Postmark.This is your code:  ".$vCode."."
+// );
+
 require_once('.\vendor\autoload.php');
 use Postmark\PostmarkClient;
 
-// Example request
+
 $client = new PostmarkClient("dd966b63-c58c-4772-8123-05e4e98acd1a");
+$fromEmail = "sender@example.org";
+$toEmail = "team@krowl.io";
+$subject = "Hello from Postmark";
+$htmlBody = "<strong>Hello</strong> dear Postmark user.";
+$textBody = "Hello dear Postmark user.";
+$tag = "example-email-tag";
+$trackOpens = true;
+$trackLinks = "None";
+$messageStream = "outbound";
 
+// Send an email:
 $sendResult = $client->sendEmail(
-  "team@krowl.io",
-   $email,
-  "Hello ".$username ." from Postmark!",
-  "This is just a friendly 'hello' from your friends at Postmark.This is your code:  ".$vCode."."
+  $fromEmail,
+  $toEmail,
+  $subject,
+  $htmlBody,
+  $textBody,
+  $tag,
+  $trackOpens,
+  NULL, // Reply To
+  NULL, // CC
+  NULL, // BCC
+  NULL, // Header array
+  NULL, // Attachment array
+  $trackLinks,
+  NULL, // Metadata array
+  $messageStream
 );
-
 
 ?>
