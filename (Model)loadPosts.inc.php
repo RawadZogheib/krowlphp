08c011e4520b7d078753ca533633f218f5cfb908
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-06-16 12:51:28 +0300

    Update (Model)loadPosts.inc.php

--- (Model)loadPosts.inc.php
+++ (Model)loadPosts.inc.php
@@ -10,7 +10,7 @@
 
 	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date`, a.`first_name`, a.`last_name`,a.`username` FROM `posts`p 
 	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."' ORDER BY p.`post_date` DESCLIMIT ".$limit[0].",".$limit[1]."";
-
+echo $sql;
 	$xx = mysqli_query($con,$sql);
 	 
 ?>
