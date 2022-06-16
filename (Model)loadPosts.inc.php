bd0ea2a9679766df95ee15354eab114fc5208f2a
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-06-16 12:35:31 +0300

    adding limit to load posts

--- (Model)loadPosts.inc.php
+++ (Model)loadPosts.inc.php
@@ -1,8 +1,15 @@
 <?php 
 	//Getting all the posts of account and account's friends that are in the same uni with him 
-	  
+	$limit=array();
+	require 'Functions.php';
+	$limit=loadLimit($count,$currentPage);
+  
+
+
+
+
 	$sql="SELECT p.`post_id`, p.`post_question`,p.`post_context`, p.`post_likes`, p.`post_tag`, p.`post_date`, a.`first_name`, a.`last_name`,a.`username` FROM `posts`p 
-	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."' ORDER BY p.`post_date` DESC";
+	join `account`a on p.`account_Id`=a.`account_Id` WHERE a.university_ids='".$user_uni."' ORDER BY p.`post_date` DESCLIMIT ".$limit[0].",".$limit[1]."";
 
 	$xx = mysqli_query($con,$sql);
 	 
