e9ca5d891e5fec88ed944553e5ca790f86784e67
Danae777 <82775861+Danae777@users.noreply.github.com>
2022-07-22 11:22:48 +0300

    Update (Model)config.inc.php

--- (Model)config.inc.php
+++ (Model)config.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-	require $_SERVER["DOCUMENT_ROOT"] . '/secureFolder/Config/Model/(Model)config.inc.php';
+	require $_SERVER["DOCUMENT_ROOT"] . '/SecureFolder/Config/Model/(Model)config.inc.php';
 
 ?>
